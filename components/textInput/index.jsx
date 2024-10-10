c4ccb86ad83ada7f5cb38a71dfa267442305b639
SumeeraSehar <sumeerasehar12345@gmail.com>
2024-10-10 16:49:58 +0500

    textinput

--- components/textInput/index.jsx
+++ components/textInput/index.jsx
@@ -2,13 +2,14 @@ import React from "react";
 
 import { strings } from "@/utils";
 
-export default function TextInput({ placeholder, classes = "", label, labelclass = "" }) {
+export default function TextInput({ placeholder, classes = "", label, labelclass = "", type = "text", value, handleChange, inputKey = "" }) {
   let className = "w-full rounded-sm border border-color-1 bg-transparent pl-[24px] py-2  focus:outline-none";
 
   return (
     <>
       <label className={`text-base font-medium ${labelclass}`}> {strings[label]}</label>
       <input placeholder={placeholder} className={`${className} ${classes}`} />
+      {/* <input type={type} value={value} placeholder={placeholder} className={`w-full rounded-[10px] border border-color-1 p-4 ${classes}`} onChange={(e) => handleChange(inputKey, e.target.value)} /> */}
     </>
   );
 }
