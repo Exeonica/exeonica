650c44e77be327b8a160187c84262fda1303aad9
bilalansri <bilalakram.1558@gmail.com>
2024-10-11 17:22:01 +0500

    blogs setup

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-const BlogDetails = ({ params }) => {
-  return <div>BlogDetails {params.id}</div>;
+import { getBlog } from "@/utils";
+
+const BlogDetails = async ({ params }) => {
+  const blog = await getBlog(params.id);
+
+  return (
+    <div>
+      <span>{blog.title}</span>
+      <div dangerouslySetInnerHTML={{ __html: blog.template }} />
+    </div>
+  );
 };
 
 export default BlogDetails;
