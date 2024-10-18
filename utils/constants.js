58f6f85a8c9aeea421146e6181a7f758fb6ea43d
bilalansri <bilalakram.1558@gmail.com>
2024-10-18 14:38:11 +0500

    blog filters

--- utils/constants.js
+++ utils/constants.js
@@ -46,7 +46,7 @@ import { Android, AppleLogo, Chat, CreditCard, Guarantee, Map, Notification, Soc
 export const navLinks = [
   { name: "Home", path: "/" },
   { name: "Services", path: "/services" },
-  // { name: "Portfolio", path: "/portfolio" },
+  { name: "Portfolio", path: "/portfolio" },
   { name: "About Us", path: "/about" },
   { name: "Blogs", path: "/blogs" },
   { name: "Careers", path: "/careers" },
