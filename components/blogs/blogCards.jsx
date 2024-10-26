39200046c8fa4190847c7f6f7f3d91239c92a26d
SumeeraSehar <sumeerasehar12345@gmail.com>
2024-10-26 13:52:16 +0500

    ..

--- components/blogs/blogCards.jsx
+++ components/blogs/blogCards.jsx
@@ -15,9 +15,9 @@ const BlogsCards = ({ cardsData, limit, params }) => {
   const renderNoBlog = () => <p className="text-xl font-semibold text-card-foreground">No Blog Available.</p>;
 
   const renderBlog = (card) => (
-    <div key={card.id} className="flex flex-col justify-between rounded-xl bg-border p-6 pb-8 shadow-lg">
-      <Link href={`/blog/${card.id}`}>
-        <div className="cursor-pointer overflow-hidden rounded-md">
+    <div key={card.id} className="flex h-full min-h-full flex-col justify-between rounded-xl bg-border p-6 pb-8 shadow-lg">
+      <Link href={`/blog/${card.id}`} className="h-full">
+        <div className="flex h-full min-h-full flex-1 cursor-pointer flex-col justify-between overflow-hidden rounded-md">
           <div className="relative min-h-[240px] w-full object-cover pb-[60%]">
             <Image src={card.blogImage} alt={card.title} fill priority className="rounded-md object-cover" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" />
           </div>
@@ -32,14 +32,14 @@ const BlogsCards = ({ cardsData, limit, params }) => {
               )}
             </div>
             <p className="mb-8 text-[16px] font-normal leading-[24px] text-color-11 hover:underline">{card.desc || ""}</p>
-          </div>
-          <div className="flex items-center">
-            <div className="relative mr-3 h-10 w-10">
-              <Image src={card.bloggerImage} alt={card.bloggerName} fill priority className="rounded-full object-cover" />
-            </div>
-            <div>
-              <p className="text-[15px] font-semibold leading-[20px] text-color-12">{card.bloggerName || ""}</p>
-              <p className="text-[14px] font-normal leading-[20px] text-color-11">{moment(card.updatedAt, "MMMM DD, YYYY [at] h:mm:ss A [UTC]Z").format("MMMM Do, YYYY") || ""}</p>
+            <div className="flex items-center">
+              <div className="relative mr-3 h-10 w-10">
+                <Image src={card.bloggerImage} alt={card.bloggerName} fill priority className="rounded-full object-cover" />
+              </div>
+              <div>
+                <p className="text-[15px] font-semibold leading-[20px] text-color-12">{card.bloggerName || ""}</p>
+                <p className="text-[14px] font-normal leading-[20px] text-color-11">{moment(card.updatedAt, "MMMM DD, YYYY [at] h:mm:ss A [UTC]Z").format("MMMM Do, YYYY") || ""}</p>
+              </div>
             </div>
           </div>
         </div>
