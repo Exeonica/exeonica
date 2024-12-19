09268c0211e2cae67b1a9ec6cc4f877697b5c089
SumeeraSehar <sumeerasehar12345@gmail.com>
2024-12-19 18:25:57 +0500

    cv url issue fixed

--- components/applicationForm/index.jsx
+++ components/applicationForm/index.jsx
@@ -141,7 +141,7 @@ const ApplicationForm = ({ title, onClose, careerId }) => {
         lastDegreeCompletionYear: formData.lastDegreeCompletionYear,
         cgpa: formData.cgpa,
         coverLetter: formData.coverLetter,
-        CVURL: url,
+        cvLink: url,
         careerId: careerId,
       };
       setModalVisible(true);
